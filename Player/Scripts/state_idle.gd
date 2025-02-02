3c6d91dc8d5786a1ef8bacea4b14e2c208429ef4
Jens Fischer <JensFZ@gmail.com>
2025-02-02 23:42:07 +0100

    attack finished

--- Player/Scripts/state_idle.gd
+++ Player/Scripts/state_idle.gd
@@ -1,6 +1,7 @@
 class_name State_Idle extends State
 
 @onready var walk: State = $"../Walk"
+@onready var attack: State = $"../Attack"
 
 
 ## Enters the state
@@ -25,4 +26,6 @@ func Physics(_delta: float) -> State:
 	
 ## input event updates state
 func HandleInput(_event: InputEvent) -> State:
+	if _event.is_action_pressed("attack"):
+		return attack
 	return null
--- Player/Scripts/state_walk.gd
+++ Player/Scripts/state_walk.gd
@@ -2,6 +2,8 @@ class_name State_Walk extends State
 
 @export var move_speed : float = 100.0
 @onready var idle: State = $"../Idle"
+@onready var attack: State = $"../Attack"
+
 
 ## Enters the state
 func Enter() -> void: 
@@ -29,4 +31,6 @@ func Physics(_delta: float) -> State:
 	
 ## input event updates state
 func HandleInput(_event: InputEvent) -> State:
+	if _event.is_action_pressed("attack"):
+		return attack
 	return null
