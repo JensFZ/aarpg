33b97f8cb935797e8538096e26d838f00d795d1a
Jens Fischer <JensFZ@gmail.com>
2025-02-02 17:45:41 +0100

    State machine angefangen

---
+++ Player/Scripts/state.gd
@@ -0,0 +1,28 @@
+class_name State extends Node
+
+## Reference to player
+static var player: Player
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+## Enters the state
+func Enter() -> void: 
+	pass
+
+## Exits the state
+func Exit() -> void:
+	pass
+
+## _process updates the state
+func Process(_delta: float) -> State:
+	return null
+
+## _physics_process updates the state
+func Physics(_delta: float) -> State:
+	return null
+	
+## input event updates state
+func HandleInput(_event: InputEvent) -> State:
+	return null
