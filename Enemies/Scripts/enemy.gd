df2570df49321621d849f5e23921a3f8798a64dd
Jens Fischer <JensFZ@gmail.com>
2025-02-08 14:43:10 +0100

    enemy reagiert nun auf Hurtbox, nicht aif Player position

--- Enemies/Scripts/enemy.gd
+++ Enemies/Scripts/enemy.gd
@@ -2,8 +2,8 @@ class_name Enemy extends CharacterBody2D
 
 @export var hp: int = 3
 signal direction_changed( new_direction : Vector2 )
-signal enemy_damaged() 
-signal enemy_destroyed()
+signal enemy_damaged( hurt_box: HurtBox ) 
+signal enemy_destroyed( hurt_box: HurtBox )
 
 const DIR_4 = [Vector2.RIGHT, Vector2.DOWN, Vector2.LEFT, Vector2.UP ]
 
@@ -75,6 +75,6 @@ func _take_damage( hurt_box: HurtBox ) -> void:
 		
 	hp -= hurt_box.damage
 	if hp > 0:
-		enemy_damaged.emit()
+		enemy_damaged.emit( hurt_box )
 	else:
-		enemy_destroyed.emit()
+		enemy_destroyed.emit( hurt_box )
