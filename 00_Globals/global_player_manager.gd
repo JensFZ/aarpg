c2a4a1ad86cd30c33850b7b02fee7a3391a4240f
Jens Fischer <JensFZ@gmail.com>
2025-02-08 23:18:00 +0100

    playerspawner funtioniert

--- 00_Globals/global_player_manager.gd
+++ 00_Globals/global_player_manager.gd
@@ -2,6 +2,7 @@ extends Node
 
 const PLAYER = preload("res://Player/player.tscn")
 var player: Player
+var player_spawned: bool = false 
 
 func _ready() -> void:
 	add_player_instance()
@@ -10,3 +11,6 @@ func add_player_instance() -> void:
 	player = PLAYER.instantiate()
 	add_child( player )
 	pass
+
+func set_player_position( _new_pos: Vector2 ) -> void:
+	player.global_position = _new_pos
