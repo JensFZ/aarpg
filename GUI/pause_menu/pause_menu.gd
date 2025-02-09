0b580ddb85025a859b63a1302f2a260d88755816
Jens Fischer <JensFZ@gmail.com>
2025-02-09 18:48:42 +0100

    pause_menu hinzugefügt

---
+++ GUI/pause_menu/pause_menu.gd
@@ -0,0 +1,11 @@
+extends CanvasLayer
+
+@onready var btn_load: Button = $ColorRect/VBoxContainer/btn_load
+@onready var btn_save: Button = $ColorRect/VBoxContainer/btn_save
+
+var is_paused : bool = false 
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+	pass # Replace with function body.
