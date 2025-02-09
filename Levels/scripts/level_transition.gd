091974011b9987b488684d3e2c83f24fee019526
Jens Fischer <JensFZ@gmail.com>
2025-02-09 01:42:08 +0100

    scene transition eingebaut

--- Levels/scripts/level_transition.gd
+++ Levels/scripts/level_transition.gd
@@ -4,7 +4,7 @@ class_name LevelTransition extends Area2D
 enum SIDE { LEFT, RIGHT, TOP, BOTTOM }
 
 @export_file( "*.tscn" ) var level
-@export var target_transition : String = "LevelTransition"
+@export var target_transition_area : String = "LevelTransition"
 
 @export_category("Collision Area Settings")
 @export_range(1,12,1, "or_greater") var size : int = 2 :
@@ -26,7 +26,47 @@ enum SIDE { LEFT, RIGHT, TOP, BOTTOM }
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
 
 func _ready() -> void:
+	_update_area()
+	
+	if Engine.is_editor_hint(): # Editor Mode und nicht im Game
+		return
+
+	monitoring = false #Abschalten, da beim Level anfang, player eine Kollision auf einer transition besitzt
+	_place_player() 
+	
+	await LevelManager.level_loaded
+	
+	monitoring = true 	
+	body_entered.connect( _player_entered )	# Kollision -> rufe _player_entered auf
 	pass
+
+func _player_entered( _p : Node2D) -> void:
+	LevelManager.load_new_level( level, target_transition_area, get_offset() )
+	pass
+	
+func _place_player() -> void:
+	if name != LevelManager.target_transition:
+		return
+		
+	PlayerManager.set_player_position( global_position + LevelManager.position_offset )
+	
+func get_offset() -> Vector2:
+	var offset : Vector2 = Vector2.ZERO
+	
+	var player_pos = PlayerManager.player.global_position
+	
+	if side == SIDE.LEFT || side == SIDE.RIGHT:
+		offset.y = player_pos.y - global_position.y 
+		offset.x = 16
+		if side == SIDE.LEFT:
+			offset.x *= -1
+	else:
+		offset.x = player_pos.x - global_position.x
+		offset.y = 16
+		if side == SIDE.TOP:
+			offset.y *= -1
+	
+	return offset
 
 func _update_area() -> void:
 	var new_rect: Vector2 = Vector2( 32 , 32 )
@@ -54,3 +94,4 @@ func _update_area() -> void:
 func _snap_to_grid() -> void:
 	position.x = round( position.x / 16 ) * 16
 	position.y = round( position.y / 16 ) * 16
+	
--- Levels/scripts/player_spwan.gd
+++ Levels/scripts/player_spwan.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 func _ready() -> void:
 	visible = false
+	
+	# Code für den Spielstart
 	if PlayerManager.player_spawned == false:
 		PlayerManager.set_player_position( global_position )
 		PlayerManager.player_spawned = true
