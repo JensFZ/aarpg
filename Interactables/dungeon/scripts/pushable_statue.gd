42d33898335dd5a37c375bb5330466f13ccc6500
Jens Fischer <JensFZ@gmail.com>
2025-02-16 19:03:32 +0100

    Update pushable_statue.gd
    pushable abschalten

--- Interactables/dungeon/scripts/pushable_statue.gd
+++ Interactables/dungeon/scripts/pushable_statue.gd
@@ -1,6 +1,7 @@
 class_name PuschableStatue extends RigidBody2D
 
 @export var push_speed : float = 30.0
+@export var pushable : bool = true
 
 var push_direction : Vector2 = Vector2.ZERO : set = _set_push_direction
 
@@ -11,7 +12,11 @@ func _physics_process(_delta: float) -> void:
 	pass
 	
 func _set_push_direction( value : Vector2 ) -> void:
-	push_direction = value
+	if pushable:
+		push_direction = value
+	else:
+		push_direction = Vector2.ZERO
+		
 	if push_direction == Vector2.ZERO:
 		audio.stop()
 	else:
