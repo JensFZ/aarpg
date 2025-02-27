ca1562977ce08ca813fd6f62b4ffbb7980fa32b6
Jens Fischer <JensFZ@gmail.com>
2025-02-27 21:25:52 +0100

    Bugfix

--- npc/scripts/npc_behavior_patrol.gd
+++ npc/scripts/npc_behavior_patrol.gd
@@ -14,6 +14,8 @@ func _ready() -> void:
 	if Engine.is_editor_hint():
 		child_entered_tree.connect( gather_patrol_locations )
 		child_order_changed.connect( gather_patrol_locations )
+		return 
+		
 	super()
 	
 	if patrol_locations.size() <= 0:
