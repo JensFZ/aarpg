1c4ed26a4a3adbd4045ce1ae2a210e4eeb07b9b7
Jens Fischer <JensFZ@gmail.com>
2025-02-27 22:02:31 +0100

    Dialog aus godot editor entfernt

--- GUI/dialog_system/scripts/dialog_system.gd
+++ GUI/dialog_system/scripts/dialog_system.gd
@@ -1,3 +1,11 @@
 @tool
 @icon("res://GUI/dialog_system/icons/star_bubble.svg")
 class_name DialogSystemNode extends CanvasLayer
+
+
+func _ready() -> void:
+	if Engine.is_editor_hint():
+		if get_viewport() is Window:
+			get_parent().remove_child( self )
+			return
+		return
