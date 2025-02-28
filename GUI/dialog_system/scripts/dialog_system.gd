c939e2468ba2eefa21d4f1dd070edd60d5ab7382
Jens Fischer <JensFZ@gmail.com>
2025-03-01 00:18:06 +0100

    typing effekt

--- GUI/dialog_system/scripts/dialog_system.gd
+++ GUI/dialog_system/scripts/dialog_system.gd
@@ -3,8 +3,16 @@
 class_name DialogSystemNode extends CanvasLayer
 
 signal finished
+signal letter_added( letter : String )
+
 
 var is_active : bool = false
+var text_in_progress : bool = false
+
+var text_speed : float = 0.02
+var text_length : int = 0
+var plain_text : String
+
 var dialog_items : Array[ DialogItem ]
 var dialog_item_index : int = 0
 
@@ -14,9 +22,8 @@ var dialog_item_index : int = 0
 @onready var portrait_sprite: Sprite2D = $DialogUI/PortraitSprite
 @onready var dialog_progress_indicator: PanelContainer = $DialogUI/DialogProgressIndicator
 @onready var dialog_progress_indicator_label: Label = $DialogUI/DialogProgressIndicator/Label
-
-
-
+@onready var timer: Timer = $DialogUI/Timer
+@onready var audio: AudioStreamPlayer = $DialogUI/AudioStreamPlayer
 
 
 func _ready() -> void:
@@ -25,18 +32,27 @@ func _ready() -> void:
 			get_parent().remove_child( self )
 			return
 		return
-		
+	timer.timeout.connect( _on_timer_timeout )
 	hide_dialog()
+	
 	pass
 
 func _unhandled_input(event: InputEvent) -> void:
 	if is_active == false:
 		return
+		
 	if (
 		event.is_action_pressed("interact") or 
 		event.is_action_pressed("attack") or
 		event.is_action_pressed("ui_accept")
 	):
+		if text_in_progress:
+			content.visible_characters = text_length
+			timer.stop()
+			text_in_progress = false
+			show_dialog_button_indicator( true )
+			return
+			
 		dialog_item_index += 1
 		if dialog_item_index < dialog_items.size():
 			start_dialog()
@@ -66,10 +82,17 @@ func hide_dialog() -> void:
 	pass
 
 func start_dialog() -> void:
-	show_dialog_button_indicator(true)
+	show_dialog_button_indicator( false )
 	var _d : DialogItem = dialog_items[ dialog_item_index ]
 	set_dialog_data( _d)
 	
+	content.visible_characters = 0
+	text_length = content.get_total_character_count()
+	plain_text = content.get_parsed_text()
+	
+	text_in_progress = true
+	
+	start_timer()	
 	pass
 	
 
@@ -87,3 +110,19 @@ func show_dialog_button_indicator( _is_visible : bool ) -> void:
 		dialog_progress_indicator_label.text = "NEXT"
 	else:
 		dialog_progress_indicator_label.text = "END"
+
+func start_timer() -> void:
+	timer.wait_time = text_speed
+	
+	timer.start()
+	pass
+
+func _on_timer_timeout() -> void:
+	content.visible_characters += 1
+	
+	if content.visible_characters <= text_length:
+		start_timer()
+	else:
+		show_dialog_button_indicator( true )
+		text_in_progress = false
+	pass
