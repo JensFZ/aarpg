448f5bdfb5705c6e4c6bf1e7eca43bced293651d
Jens Fischer <JensFZ@gmail.com>
2025-03-02 00:37:30 +0100

    theme hinzugefügt und Branches

--- Interactables/dialog/scripts/dialog_choice.gd
+++ Interactables/dialog/scripts/dialog_choice.gd
@@ -2,4 +2,22 @@
 @icon("res://GUI/dialog_system/icons/question_bubble.svg")
 class_name DialogChoice extends DialogItem
 
-@export var choices : Array[String] = ["Yes", "No"]
+var dialog_branches : Array [ DialogBranch ]
+
+
+func _get_configuration_warnings() -> PackedStringArray:
+	if _check_for_dialog_branches() == false:
+		return ["Requires at least 2 DialogBranch nodes."]
+	else:
+		return []
+		
+func _check_for_dialog_branches() -> bool:
+	var _count : int = 0
+	
+	for c in get_children():
+		if c is DialogBranch:
+			_count += 1
+			if _count > 1:
+				return true
+			
+	return false 
